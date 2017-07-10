eab06d3231c7c17a86c824a4b69743acf327336c
aryaravi <aryaprasadr@gmail.com>
2017-07-10 12:26:34 -0500

    initial commit - test and resources packages only

---
+++ src/main/java/com/saika/demo/AddEx.java
@@ -0,0 +1,21 @@
+package com.saika.demo;
+
+public class AddEx  {
+	
+	static int add(int a,int b){
+		return a+b;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+       
+       int sum=AddEx.add(4,5);
+       System.out.println("ADD =" + sum);
+
+	
+		
+	}
+
+}
